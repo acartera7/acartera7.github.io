d1efc67f001bf989d2748b218c75c824dfbcd375
Andrei Cartera <andrei_cartera@yahoo.com>
2024-06-10 15:19:50 -0700

    Added a navbar at the top, added more logos added new sections.

---
+++ javascript/navbarscrollhighlight.js
@@ -0,0 +1,29 @@
+// Get all navbar buttons and sections
+const navbarButtons = document.querySelectorAll('.navbar-button');
+const sections = document.querySelectorAll('section');
+
+// Function to remove 'highlight' class from all buttons
+function removeActive() {
+  navbarButtons.forEach(button => {
+    button.classList.remove('active');
+  });
+}
+
+function addActive(element) {
+  removeActive()
+  element.classList.add('active');
+}
+
+// Function to add 'highlight' class to the active button based on scroll position
+function highlightActiveButton() {
+  let index = sections.length;
+
+  while(--index && window.scrollY < sections[index].offsetTop) {}
+
+  removeActive();
+  navbarButtons[index].classList.add('active');
+}
+
+// Event listener for scroll event
+window.addEventListener("scroll", highlightActiveButton);
+
