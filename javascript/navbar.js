669bc845729bfd5c054681284d40f8ab3373e2a6
Andrei Cartera <andrei_cartera@yahoo.com>
2024-06-17 12:58:47 -0700

    Finished navbar hamburger funcitonality, changed logo font

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,3 +1,25 @@
+$(document).ready(function() {
+  $("body").on('click', '.top .hamburger .fa.fa-bars', function() {
+    $("nav.menu").toggleClass("menu_show");
+  });
+});
+
+$(document).ready(function() {
+  function checkViewportSize() {
+      if ($(window).width() <= 600) {
+          $('.navbar-button').addClass('small');w
+      } else {
+          $('.navbar-button').removeClass('small');
+      }
+  }
+
+  // Call the function when the document is ready
+  checkViewportSize();
+
+  // Also call the function whenever the window is resized
+  $(window).resize(checkViewportSize);
+});
+
 // Get all navbar buttons and sections
 const navbarButtons = document.querySelectorAll('.navbar-button');
 const sections = document.querySelectorAll('section');
@@ -11,7 +33,10 @@ function removeActive() {
 
 function addActive(element) {
   removeActive()
-  element.classList.add('active');
+  if(element.classList.contains('small')) {
+    $("nav.menu").toggleClass("menu_show");
+  } else
+    element.classList.add('active');
 }
 
 // Function to add 'highlight' class to the active button based on scroll position
@@ -24,6 +49,9 @@ function highlightActiveButton() {
   navbarButtons[index].classList.add('active');
 }
 
+
+
 // Event listener for scroll event
 window.addEventListener("scroll", highlightActiveButton);
 
+$(window).resize(checkViewportSize);
